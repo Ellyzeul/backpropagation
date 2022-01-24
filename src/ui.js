a5720a7683f75325a03afaad22165ef6ceb25fc0
Ellyzeul <gabrielaug.240@gmail.com>
2022-01-24 00:29:22 -0300

    feat: initial interface

---
+++ src/ui.js
@@ -0,0 +1,26 @@
+// Init
+
+const stopConditions = document.querySelectorAll('input[name="stop_condition"]')
+stopConditions[0]
+    .addEventListener('change', () => document.querySelector('#iteration_number').disabled = true)
+stopConditions[1]
+    .addEventListener('change', () => document.querySelector('#iteration_number').disabled = false)
+
+const stopInput = document.querySelector('#iteration_number')
+stopInput.disabled = true
+stopInput.value = ''
+
+
+const hiddenNeurons = document.querySelectorAll('input[name="hidden_neuron_number"]')
+hiddenNeurons[0]
+    .addEventListener('change', () => document.querySelector('#user_neuron_number').disabled = true)
+hiddenNeurons[1]
+    .addEventListener('change', () => document.querySelector('#user_neuron_number').disabled = false)
+
+hiddenNeurons[0].checked = true
+
+const neuronInput = document.querySelector('#user_neuron_number')
+neuronInput.disabled = true
+neuronInput.value = ''
+
+
